46ebb470816baf0c1f4560f871d5c7371c7eb01c
SolarBear <7608394+SolarBear@users.noreply.github.com>
2022-12-02 22:06:19 -0500

    trimmed a bit of fat

--- day2.rb
+++ day2.rb
@@ -2,41 +2,16 @@ LOSS = 0
 DRAW = 3
 WIN = 6
 
-MAP = {
-  'A' => :rock,
-  'B' => :paper,
-  'C' => :scissors,
-  'X' => :rock,
-  'Y' => :paper,
-  'Z' => :scissors
-}
+MOVE_POINTS = { rock: 1, paper: 2, scissors: 3 }.freeze
 
-WINS_1 = {
-  rock: :paper,
-  paper: :scissors,
-  scissors: :rock
-}
-
-WINS_2 = {
-  'X' => LOSS,
-  'Y' => DRAW,
-  'Z' => WIN
-}
-
-MOVE_POINTS = {
-  rock: 1,
-  paper: 2,
-  scissors: 3
-}
-
-ORDER = %i[scissors paper rock].freeze
+ORDER = %i[rock paper scissors].freeze
 
 def rps_win(move1, move2)
   return DRAW if move1 == move2
 
   idx = ORDER.index(move1)
 
-  return move2 == ORDER[idx - 1] ? WIN : LOSS
+  return move2 == ORDER[(idx + 1) % ORDER.length] ? WIN : LOSS
 end
 
 def score(move1, move2)
@@ -49,23 +24,24 @@ total2 = 0
 
 file.readlines.each do |line|
   opponent, me = line.split(' ')
-  move1 = MAP[opponent]
-  move2 = MAP[me]
+  move1 = ORDER[opponent.ord - 'A'.ord]
+  move2 = ORDER[me.ord - 'X'.ord]
 
   total1 += score(move1, move2)
 
   idx = ORDER.index(move1)
   case me
   when 'X'
-    move2 = ORDER[(idx + 1) % ORDER.length]
+    move2 = ORDER[idx - 1]
   when 'Y'
     move2 = move1
   when 'Z'
-    move2 = ORDER[idx - 1]
+    move2 = ORDER[(idx + 1) % ORDER.length]
   end
 
   total2 += score(move1, move2)
 end
 
-puts total1 # 12772
-puts total2 # 11618
+puts "Oops! total1 should be 12772, not #{total1}" if total1 != 12_772
+
+puts "Oops! total2 should be 11618, not #{total2}" if total2 != 11_618
