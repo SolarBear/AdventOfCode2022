051e3f8ec41ff86201d6472f36254bf56ee9b72a
SolarBear <7608394+SolarBear@users.noreply.github.com>
2022-12-11 18:27:48 -0500

    Day 11, part 1 done. Part 2 will require a bit more thinking.

---
+++ day11.rb
@@ -0,0 +1,90 @@
+class Monkey
+  attr_reader :inspections, :items
+
+  def initialize(worry_level)
+    @worry_level = worry_level
+    @items = []
+    @operation = nil
+    @op_arg = nil
+    @test = nil
+    @test_true_target = nil
+    @test_false_target = nil
+    @inspections = 0
+  end
+
+  def perform_turn(monkeys)
+    items = @items.dup
+    items.each do |item|
+      item = inspect(item)
+      item /= @worry_level unless @worry_level == 1
+
+      target = if (item % @test).zero?
+                 @test_true_target
+               else
+                 @test_false_target
+               end
+
+      throw_item(monkeys[target], item)
+    end
+
+    @items = []
+  end
+
+  def inspect(item)
+    @inspections += 1
+    arg = if @op_arg == 'old'
+            item
+          else
+            @op_arg.to_i
+          end
+
+    if @operation == '+'
+      item += arg
+    elsif @operation == '*'
+      item *= arg
+    end
+
+    item
+  end
+
+  def throw_item(monkey, item)
+    monkey.catch_item(item)
+  end
+
+  def catch_item(item)
+    @items << item
+  end
+
+  def load(lines)
+    lines.shift
+    _, items = lines.shift.split(':').map(&:strip)
+    @items = items.split(',').map(&:to_i)
+
+    _, op = lines.shift.strip.split('old ')
+    @operation, @op_arg = op.split(' ')
+
+    @test = lines.shift.split('by ')[1].to_i
+    @test_true_target = lines.shift.split('monkey ')[1].strip.to_i
+    @test_false_target = lines.shift.split('monkey ')[1].strip.to_i
+  end
+end
+
+def rounds(nb, monkeys_data, worry_level)
+  monkeys = Array.new (monkeys_data.length) { Monkey.new(worry_level) }
+  monkeys.each_index { |i| monkeys[i].load(monkeys_data[i].split("\n")) }
+
+  nb.times do
+    monkeys.each do |m|
+      m.perform_turn(monkeys)
+    end
+  end
+
+  mb = monkeys.map(&:inspections).max(2)
+  mb[0] * mb[1]
+end
+
+file = File.open('input11.txt')
+monkeys_data = file.read.split("\n\n")
+
+puts rounds(20, monkeys_data, 3) # 78678
+# puts rounds(1_000, monkeys_data, 1)
